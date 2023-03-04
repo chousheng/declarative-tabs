3c40de6f8c6b4a61e89c33bf9e2dbd1b3ca4f318
Sheng Chou <chousheng@gmail.com>
2023-03-03 18:25:57 -0800

    Implement simple-tabs-declarative

--- simple-tabs.js
+++ simple-tabs.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+const initSimpleTabs = () => {
   // Iterate tabs array
   let tabsCollection = document.getElementsByClassName("tabs");
   [...tabsCollection].forEach((tabs) => {
@@ -30,4 +30,52 @@ window.onload = function () {
     tabArray[0].classList.add("active");
     tabContentArray[0].classList.add("active");
   });
+};
+
+const initSimpleTabsDeclarative = () => {
+  // Build tab groups
+  let nodeList = document.querySelectorAll("[class$=-tab]")
+  let groups = [];
+  if (nodeList.length > 0) {
+      let group = [nodeList[0]];
+      let firstNode = nodeList[0];
+      for (let i = 1; i < nodeList.length; i++) {
+          if (firstNode.parentNode == nodeList[i].parentNode) {
+              group.push(nodeList[i]);
+          } else {
+              groups.push(group);
+              group = [nodeList[i]];
+              firstNode = nodeList[i];
+          }
+      }
+      groups.push(group);
+  }
+
+  // Add tab elements to tabs elements and CSS properties
+  groups.forEach((group) => {
+    let tabsElement = group[0].parentNode;
+    tabsElement.classList.add("tabs");
+
+    let tabArray = [];
+    group.forEach((tabContent) => {
+      tabContent.classList.add("tab-content");
+
+      let tab = document.createElement('div');
+      tabContent.classList.forEach((className) => {
+        let name = className.match(/(.*)-tab$/);
+        if (name) {
+          tab.textContent = name[1];
+          tabContent.classList.remove(name[0]);
+        }
+      });
+      tab.className = 'tab';
+
+      tabArray.push(tab);
+    });
+
+    tabsElement.prepend(...tabArray);
+  });
+
+  // Initialize the tabs
+  initSimpleTabs();
 };
